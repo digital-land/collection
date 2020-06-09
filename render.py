1de53998cca75d109c304170581edc80fab606ea
Colm Britton <colmjude@gmail.com>
2020-06-09 15:00:28 +0100

    Use correct date format

--- render.py
+++ render.py
@@ -66,7 +66,7 @@ def render(path, template, **kwargs):
 
 render("index.html", collections_template, data=collections_data)
 render("log/index.html", collections_log_template, data=collections_log_data)
-render("log/04-06-2020/index.html", collections_log_date_template, data=collections_log_date_data)
+render("log/2020-06-04/index.html", collections_log_date_template, data=collections_log_date_data)
 # for collection in collection_data:
 #     render(f"{collection['collection']}/index.html", a_collection_template, data=collection)
 # render("brownfield-land/log/index.html", a_collection_log_template, data=brownfield_collection_log_data)
